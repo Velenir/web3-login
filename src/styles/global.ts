13670e6c1a22d353eb34b7e7b754cb7d57905f73
Velenir <velenir@gmail.com>
2022-11-27 20:51:56 +0100

    move more styles to global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,51 +12,54 @@ export const globals = css`
       box-sizing: inherit;
     }
 
-    /* body {
-      background-color: #cecece;
-    } */
+  :root {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
 
-    #root {
-      max-width: 1280px;
-      width: 100%;
-      margin: 0 auto;
-      padding: 2rem;
-    }
+    color-scheme: light dark;
+    color: rgba(255, 255, 255, 0.87);
+    background-color: #242424;
 
-    .logo {
-      height: 6em;
-      padding: 1.5em;
-      will-change: filter;
-    }
-    .logo:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.react:hover {
-      filter: drop-shadow(0 0 2em #61dafbaa);
-    }
-
-    @keyframes logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    @media (prefers-reduced-motion: no-preference) {
-      a:nth-of-type(2) .logo {
-        animation: logo-spin infinite 20s linear;
-      }
-    }
-
-    .card {
-      padding: 2em;
-    }
-
-    .read-the-docs {
-      color: #888;
-    }
-
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
   }
-`;
+
+  body {
+    margin: 0;
+    display: flex;
+    place-items: center;
+    min-width: 320px;
+    min-height: 100vh;
+  }
+
+  h1 {
+    font-size: 3.2em;
+    line-height: 1.1;
+  }
+
+  #root {
+    max-width: 1280px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 2rem;
+  }
+
+  @media (prefers-color-scheme: light) {
+    :root {
+      color: #213547;
+      background-color: #ffffff;
+    }
+    a:hover {
+      color: #747bff;
+    }
+    button {
+      background-color: #f9f9f9;
+    }
+  }
+
+}`;
