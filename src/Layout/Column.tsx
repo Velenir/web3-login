246be263bc652248a3939073869816546f43cd24
Velenir <velenir@gmail.com>
2022-11-27 20:42:37 +0100

    Root styled

---
+++ src/Layout/Column.tsx
@@ -0,0 +1,7 @@
+import {styled} from "@linaria/react"
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
---
+++ src/Layout/Root.tsx
@@ -0,0 +1,5 @@
+import { styled } from "@linaria/react"
+
+export const RootContainer = styled.div`
+  width: min(500px, 100%)
+`
