f49d0f21161b9963538cecdc9c2ca62b7e1035b9
Velenir <velenir@gmail.com>
2022-11-27 21:26:21 +0100

    fix WConnect & Coinbase deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import linaria from '@linaria/rollup';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,20 @@ export default defineConfig({
     babelOptions: {
       presets: ['@babel/preset-typescript', '@babel/preset-react'],
     },
-  }), react(),]
+  }), react(),],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        })
+      ]
+    }
+  }
 })
